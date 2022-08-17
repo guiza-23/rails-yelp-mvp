2a65fbe699ed6c20cd36b80a47bbfd6d396f836b
Ignacio Zanotto <ignaciozanotto@gmail.com>
2022-08-17 17:45:36 -0300

    casi yeko mvp

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+  #  get 'restaurants/:id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
+  # post 'restaurants/:id/reviews', to: 'reviews#create'
+
+  # get 'restaurants/:id', to: 'restaurants#show', as: :reviews
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'seeding'
+Restaurant.create(name: 'dragon', address: 'talcahuano 123', category: 'chinese')
+Restaurant.create(name: 'croasant', address: 'paris 123', category: 'french')
+Restaurant.create(name: 'pasta bella', address: 'torre pizza 123', category: 'italian')
+Restaurant.create(name: 'Brusela', address: 'quesey 123', category: 'belgian', phone_number: '15-32029454')
+Restaurant.create(name: 'arrozito', address: 'suzuki 123', category: 'japanese')
+
+puts 'seeding complete'
